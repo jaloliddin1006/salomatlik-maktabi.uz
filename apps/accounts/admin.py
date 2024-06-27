bcd6d61a4a952bf679f1a087da32bd3f6b484b97
Ogabek <toshniyozovogabek0627@gmail.com>
2024-06-27 08:59:12 -0700

    some changes on user model

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from apps.accounts.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+            (
+                None,
+                {
+                    "classes": ("wide",),
+                    "fields": ("email", "password1", "password2", "username"),
+                },
+            ),
+        )
