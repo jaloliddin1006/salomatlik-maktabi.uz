85221887f59349cac355e1cae9409538d525ad90
Jaloliddin <jmamatmusayev@gmail.com>
2024-06-27 18:37:08 +0500

    create user model and change gitignore

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
- 
+class User(AbstractUser):
+    email = models.EmailField(null=True, blank=True)
+    
+    
+    def __str__(self):
+        return self.username
