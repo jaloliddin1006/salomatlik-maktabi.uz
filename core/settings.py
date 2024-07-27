3a75e559750da11258d2ac4052f20178d9fd3c95
Jaloliddin <jmamatmusayev@gmail.com>
2024-07-27 19:30:38 +0500

    chend removed

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-476-2%-51k&8k+&5b3^+!ulm*9^v=((+ci%41&f5dp^@c)!a+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'#Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
